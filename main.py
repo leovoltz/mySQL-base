54660abd536a1f5fab4b125c92f498010712f5c1
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-10-03 18:22:35 -0300

    testando criação de banco de dados e comandos

--- main.py
+++ main.py
@@ -2,16 +2,54 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
+import os
+
 import pymysql
+import dotenv
+
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
 
 connection = pymysql.connect(
-    host='localhost',
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
+    host=os.environ['MYSQL_HOST'],
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
 
 with connection:
     with connection.cursor() as cursor:
-        # SQL
-        print(cursor)
+        cursor.execute(
+            f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
+            'id INT NOT NULL AUTO_INCREMENT, '
+            'nome VARCHAR(50) NOT NULL, '
+            'idade INT NOT NULL, '
+            'PRIMARY KEY (id)'
+            ')'
+        )
+        # Warning: Truncate limpa a tabela
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
+    connection.commit()
+
+    # Começa a manipular os dados a partir daqui:
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES (%s, %s) '
+        )
+        data = ('Luiz', 18)
+        result = cursor.execute(sql, data)
+        print(result)
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) VALUES (%(nome)s, %(idade)s) '
+        )
+        data2 = {
+            "nome": "Jorge", "idade": 15
+        }
+        result = cursor.execute(sql, data2)
+    connection.commit()
